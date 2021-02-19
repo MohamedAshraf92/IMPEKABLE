cd305e9239da0cd7c81628dce91cb318ecc63431
MohamedAshraf92 <eng.mohamed.hfs@gmail.com>
2021-02-19 15:34:36 +0200

    edit readme

--- src/containers/profile/profile.js
+++ src/containers/profile/profile.js
@@ -9,14 +9,17 @@ import { FaUserTag } from 'react-icons/fa'
 import { FcTodoList } from 'react-icons/fc'
 import PostCard from '../../components/postCard/postCard'
 import Album from '../../components/album/album'
+import Todo from '../../components/todo/todo'
 
 const Profile = (props) => {
 
-    const visitedUserID =  props.match.params.id
+    const visitedUserID =  parseInt(props.match.params.id)
 
     const [visitedUser, setVisitedUser] = useState([])
-    const [visitedPosts, setVisitedPosts] = useState([])
+    const [posts, setPosts] = useState([])
     const [albums, setAlbums] = useState([])
+    const [todos, setTodos] = useState([])
+    const [renderedComponent, setRenderedComponent] = useState(null)
 
     const getUser = () => {
         axios.get(`/users/${visitedUserID}`).then(res => {
@@ -26,7 +29,7 @@ const Profile = (props) => {
 
     const getPosts = () => {
         axios.get('/posts').then(res => {
-            setVisitedPosts(res.data.filter(post => post.userId === visitedUserID))
+            setPosts(res.data.filter(post => post.userId === visitedUserID))
         }).catch(err => window.alert(err))
     }
 
@@ -36,13 +39,24 @@ const Profile = (props) => {
         }).catch(err => window.alert(err))
     }
 
+    const getTodos = () => {
+        axios.get('/todos').then(res => {
+            setTodos(res.data.filter(album => album.userId === visitedUserID))
+        }).catch(err => window.alert(err))
+    }
+
     useEffect(() => {
         getUser()
         getPosts()
         getAlbums()
+        getTodos()
     }, [])
-
-    const renderPosts = visitedPosts.map(post => {
+    
+    useEffect(() => {
+        setRenderedComponent(renderAlbums)
+    }, [albums])
+    
+    const renderPosts = posts.map(post => {
         return (
             <PostCard 
                 key={post.id}
@@ -62,6 +76,12 @@ const Profile = (props) => {
         )
     })
 
+    const renderTodos = todos.map(todo => {
+        return(
+            <Todo key={todo.id} title={todo.title} />
+        )
+    })
+
     return (
         <div className='background-profile'>
             <div className='profile'>
@@ -73,31 +93,22 @@ const Profile = (props) => {
                 </div>
                 <h3>{visitedUser.name}</h3>
                 <div className='tabs'>
-                    <div className='tab'>
+                    <div className='tab' onClick={() => setRenderedComponent(renderAlbums)}>
                         <IoMdImages className='icon-tab-photos' /> &nbsp; <p>Photo/Video</p>
                     </div>
-                    <div className='tab'>
+                    <div className='tab' onClick={() => setRenderedComponent(renderPosts)}>
                         <FaUserTag className='icon-tab-posts' /> &nbsp; <p>Posts</p>
                     </div>
-                    <div className='tab'>
+                    <div className='tab' onClick={() => setRenderedComponent(renderTodos)}>
                         <FcTodoList className='icon-tab-todos' /> &nbsp; <p>To-dos</p>
                     </div>
                 </div>
-                <div className='render-component'>
-                    <div className='render-posts'>
-                        {renderPosts}
-                    </div>
-                    <div className='render-albums'>
-                        {renderAlbums}
-                    </div>
-                    <div className='render-todos'>
-                    </div>
+                <div className='rapper'>
+                    {renderedComponent}
                 </div>
-                
             </div>
         </div>
     );
 };
 
 export default Profile;
-// {componentToRender}
